8e9a52a521ec7b93f0b30296c12e7690b51f505e
yassine.alaoui <yassine.alaoui@sully-group.fr>
2018-10-23 09:52:37 +0200

    Initial empty project

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "ns-home",
+    moduleId: module.id,
+    templateUrl: "./item-detail.component.html",
+})
+export class HomeComponent implements OnInit {
+
+    constructor(
+    ) { }
+
+    ngOnInit(): void {  }
+}
